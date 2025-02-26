f628718f66626565ad5c0a46300a7d0def456a1b
philj <phil@ou.fr>
2025-02-26 19:33:19 +0100

    fix

--- inkycal/modules/inkycal_bus_tcl.py
+++ inkycal/modules/inkycal_bus_tcl.py
@@ -36,7 +36,7 @@ def get_prochaines_horaires(ligne, arret):
             return list(filter(None, map(extract_time, response.json())))
         else:
             print(f"Erreur {response.status_code}: {response.text}")
-            return [f"Erreur Ligne {ligne}/ Arrêt {arret}"]
+            return [f"Erreur {ligne}/ {arret}"]
 
     except requests.RequestException as e:
         print(f"Une erreur est survenue : {e}")
